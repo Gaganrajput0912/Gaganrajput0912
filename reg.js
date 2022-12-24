b6d048d70794041b74832e3119a01c9820f7f836
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2022-12-24 18:27:45 +0530

    first commit

--- reg.js
+++ reg.js
@@ -10,6 +10,22 @@ window.onload = function () {
             document.getElementById("number").value,
             document.getElementById("date").value,
             document.getElementById("time").value)
+
+
     });
+}
+
+function localS(e) {
+    const name = e.target.username.value
+    const email = e.target.email.value
+    const number = e.target.number.value
+    const date = e.target.date.value
+    const time = e.target.time.value;
+
+    localStorage.setItem('name', name)
+    localStorage.setItem('email', email)
+    localStorage.setItem('number', number)
+    localStorage.setItem('date', date)
+    localStorage.setItem('time', time)
 }
 
